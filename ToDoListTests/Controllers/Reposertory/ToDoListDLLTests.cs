73bd915a8472b4ce385005039d8728594f2877b2
chuangmaster <chuangmaster@github.com>
2017-07-24 01:46:05 +0800

    TDD/init

---
+++ ToDoListTests/Controllers/Reposertory/ToDoListDLLTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoListLibrary.Controllers.Reposertory.ToDoList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoListLibrary.Controllers.Reposertory.ToDoList.Tests
+{
+    [TestClass()]
+    public class ToDoListDLLTests
+    {
+        [TestMethod()]
+        public void InsertContentTest()
+        {
+            ToDoListDLL todoDll = new ToDoListDLL();
+            todoDll.InsertContent("abc");
+            //Assert.Fail();
+        }
+    }
+}
