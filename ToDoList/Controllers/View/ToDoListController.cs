61e02bea82ec690786635e8bbeee7eab11cb65cb
chuangmaster <chuangmaster@github.com>
2017-07-25 00:49:31 +0800

    master/ init view

--- ToDoList/Controllers/View/ToDoListController.cs
+++ ToDoList/Controllers/View/ToDoListController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using ToDoList.Models;
+using ToDoListLibrary.Controllers.Service.ToDoList;
+using ToDoListLibrary.Entity.ToDoList;
 namespace ToDoList.Controllers
 {
     public class ToDoListController : Controller
@@ -11,7 +13,11 @@ namespace ToDoList.Controllers
         // GET: ToDoList
         public ActionResult Index()
         {
-            return View();
+            ToDoListBLL todoListBLL = new ToDoListBLL();
+            List<ContentEntity> contentList = todoListBLL.GetListContentEntity();
+            ToDoListModel model = new ToDoListModel();
+            model.entityList = contentList;
+            return View(model);
         }
     }
 }
