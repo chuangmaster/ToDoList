2f436181be221ca9957a75a2a3e720da3b0c6ee1
chuangmaster <chuangmaster@users.noreply.github.com>
2017-07-24 09:57:00 +0800

    Tdd (#1)
    * ToDoListLib/ remove some uesless project.
    
    * ToDoListLib/ modified DAL and add connectString into web.config
    
    * TDD/init
    
    * ToDoListLib/add a mothod ToDoListLib.GetConnectString
    
    * TDD/modified connectString

--- ToDoList/Controllers/Reposertory/ToDoListDLL.cs
+++ ToDoList/Controllers/Reposertory/ToDoListDLL.cs
@@ -5,6 +5,9 @@ using System.Web;
 using System.Data.Sql;
 using System.Data.SqlClient;
 using ToDoListLibrary.Entity.ToDoList;
+using System.Web.Configuration;
+using System.Data;
+
 namespace ToDoListLibrary.Controllers.Reposertory.ToDoList
 {
     /// <summary>
@@ -12,23 +15,143 @@ namespace ToDoListLibrary.Controllers.Reposertory.ToDoList
     /// </summary>
     public class ToDoListDLL
     {
-        public ContentEntity ReadEntity()
+        /// <summary>
+        /// Read one content entity
+        /// </summary>
+        /// <param name="fld_ID"></param>
+        /// <returns></returns>
+        public ContentEntity ReadEntity(string fld_ID)
         {
-            return new ContentEntity();
+            ContentEntity entity = null;
+            string strSql = "SELECT Top 1 *  FROM tb_ListContent WITH(NO LOCK) WHERE fld_ID=@fld_ID ";
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = GetConnectString();
+                conn.Open();
+                try
+                {
+                    SqlCommand cmd = new SqlCommand(strSql);
+                    SqlParameter parameter = new SqlParameter();
+                    cmd.Connection = conn;
+                    cmd.Parameters.Add(new SqlParameter("@fld_ID", Convert.ToInt32(fld_ID)));
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if (dr.Read())
+                    {
+                        entity.fld_ID = dr["fld_ID"].ToString();
+                        entity.fld_Content = dr["fld_Content"].ToString();
+                        entity.fld_CreateDate = DateTime.Parse(dr["fld_ID"].ToString());
+                    }
+                }
+                catch (Exception)
+                {
+                    // handle Exception
+                }
+            }
+            return entity;
         }
+        /// <summary>
+        /// Read one content entity
+        /// </summary>
+        /// <returns></returns>
         public List<ContentEntity> ReadAllContentEntity()
         {
-            return new List<ContentEntity>( );
+            List<ContentEntity> listContentEntity = new List<ContentEntity>();
+            string strSql = "SELECT * FROM tb_ListContent WITH(NO LOCK)";
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = GetConnectString();
+                conn.Open();
+                try
+                {
+                    SqlCommand cmd = new SqlCommand(strSql);
+                    cmd.Connection = conn;
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        ContentEntity entity = new ContentEntity();
+                        entity.fld_ID = dr["fld_ID"].ToString();
+                        entity.fld_Content = dr["fld_Content"].ToString();
+                        entity.fld_CreateDate = DateTime.Parse(dr["fld_ID"].ToString());
+                        listContentEntity.Add(entity);
+                    }
+                }
+                catch (Exception)
+                {
+                    // handle Exception
+                }
+            }
+            return listContentEntity;
         }
-
-        public bool InsertContent()
+        /// <summary>
+        /// Insert a Todo content
+        /// </summary>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public bool InsertContent(string content)
         {
-            return false;
+            bool isSuccess = false;
+            List<ContentEntity> listContentEntity = new List<ContentEntity>();
+            string strSql = "INSERT INTO tb_ListContent (fld_Content) VALUES(@fld_Content)";
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = GetConnectString();
+                conn.Open();
+                try
+                {
+                    SqlCommand cmd = new SqlCommand(strSql);
+                    cmd.Parameters.Add(new SqlParameter("@fld_Content", content));
+                    cmd.Connection = conn;
+                     
+                    if (cmd.ExecuteNonQuery()>0)
+                    {
+                        isSuccess = true;
+                    }
+                }
+                catch (Exception)
+                {
+                    // handle Exception
+                }
+            }
+            return isSuccess;
         }
-
+        /// <summary>
+        /// delete a Todo content
+        /// </summary>
+        /// <param name="fld_ID"></param>
+        /// <returns></returns>
         public bool DeleteContent(string fld_ID)
         {
-            return false;
+            bool isSuccess = false;
+            List<ContentEntity> listContentEntity = new List<ContentEntity>();
+            string strSql = "DELETE tb_ListContent WHERE fld_ID=@fld_ID ";
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = GetConnectString();
+                conn.Open();
+                try
+                {
+                    SqlCommand cmd = new SqlCommand(strSql);
+                    cmd.Connection = conn;
+                    SqlParameter parameter = new SqlParameter();
+                    cmd.Parameters.Add(new SqlParameter("@fld_ID", Convert.ToInt32(fld_ID)));
+                    
+                    if (cmd.ExecuteNonQuery()>0)
+                    {
+                        isSuccess = true;
+                    }
+                }
+                catch (Exception)
+                {
+                    // handle Exception
+                }
+            }
+            return isSuccess;
+        }
+
+        private string GetConnectString()
+        {
+            return WebConfigurationManager.ConnectionStrings["connectString"].ConnectionString; 
         }
     }
 }
+
--- ToDoList/Controllers/Service/ToDoListBLL.cs
+++ ToDoList/Controllers/Service/ToDoListBLL.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using ToDoListLibrary.Entity.ToDoList;
 
 namespace ToDoListLibrary.Controllers.Service.ToDoList
 {
     public class ToDoListBLL
     {
+        public List<ContentEntity> GetListContentEntity()
+        {
+
+            return new List<ContentEntity>();
+        }
+
+        public bool AddContent()
+        {
+            return false;
+        }
+
+        public bool RemoveContent(string fld_ID)
+        {
+            return false;
+        }
     }
 }
