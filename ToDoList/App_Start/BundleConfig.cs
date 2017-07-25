5076f6f46ec67c8a01c666e6270125e036e439b2
chuangmaster <chuangmaster@github.com>
2017-07-25 23:40:36 +0800

    master/modified BundleConfig, view and add css

--- ToDoList/App_Start/BundleConfig.cs
+++ ToDoList/App_Start/BundleConfig.cs
@@ -23,6 +23,10 @@ namespace ToDoList
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/ToDoList").Include(
+                    "~/Scripts/ToDoList.js"));
+            
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
